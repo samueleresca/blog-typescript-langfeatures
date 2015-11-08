30140674130d5984d2d28d6c22ed696e057a0ad3
Samuele Resca <samuele.resca@gmail.com>
2015-11-08 13:03:32 +0100

    Creating Car and Vehicle classes

--- Scripts/Models/Car.ts
+++ Scripts/Models/Car.ts
@@ -1,16 +1,31 @@
 /// <reference path="Vehicle.ts"/>
-class Car extends Vehicle{
-	private meo: string;
-	constructor( name: string){
-		super(name);
-		this.meo=name;
+class Car extends Vehicle {
+
+	private _car_insurance: string;
+
+	private _license_plate: string;
+
+	constructor(brand: string, height: number, weight: number, car_insurance: string, license_plate: string) {
+		super(brand, height, weight);
+		this._car_insurance = car_insurance;
+		this._license_plate = license_plate;
 	}
-	
-	public test(){
-		alert(meo.toString());
+
+	get getCarInsurance() {
+		return this._car_insurance;
 	}
+
+	set setCarInsurance(car_insurance: string) {
+		this._car_insurance = car_insurance;
+	}
+
+
+	get getLicensePlate() {
+		return this._license_plate;
+	}
+
+	set setLicensePlate(license_plate: string) {
+		this._license_plate = license_plate;
+	}
+
 }
-
-var meo = new Car("test");
-
-meo.test();
--- Scripts/Models/Vehicle.ts
+++ Scripts/Models/Vehicle.ts
@@ -1,3 +1,39 @@
-class Vehicle{
-	constructor(protected name:string){}
+class Vehicle {
+	private _brand: string;
+	private _height: number;
+	private _weight: number;
+
+
+	constructor(brand: string, height: number, weight: number) {
+		this._brand = brand;
+		this._height = height;
+		this._weight = weight
+	}
+
+	get getBrand() {
+		return this._brand;
+	}
+
+	set setBrand(brand: string) {
+		this._brand = brand;
+	}
+
+	get getHeight() {
+		return this._height;
+	}
+
+	set setHeight(height: number) {
+		this._height = height;
+	}
+
+
+	get getWeight() {
+		return this._weight;
+	}
+
+	set setWeight(weight: number) {
+		this._weight = weight;
+	}
+
+
 }
